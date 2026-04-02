9e27f591a9bfa163d08b6ec8ff44888272ff6270
Antonio-el-Bear <antoniozambuko@gmail.com>
2026-04-02 12:28:01 +0200

    Remove simple validation function for login form from scripts.js

--- Invoice-System-In-PHP-main/js/scripts.js
+++ Invoice-System-In-PHP-main/js/scripts.js
@@ -831,28 +831,4 @@ $(document).ready(function() {
 	    return errorCounter;
 	}
 
-	// Simple validation for login form
-	function validateForm() {
-	    var errorCounter = 0;
-	    var $username = $("#username");
-	    var $password = $("#password, input[name='password']").first();
-	    var username = ($username.val() || "").trim();
-	    var password = ($password.val() || "").trim();
-
-	    if (username === "") {
-	        errorCounter++;
-	        $username.addClass("input-error");
-	    } else {
-	        $username.removeClass("input-error");
-	    }
-
-	    if (password === "") {
-	        errorCounter++;
-	        $password.addClass("input-error");
-	    } else {
-	        $password.removeClass("input-error");
-	    }
-
-	    return errorCounter;
-	}
 });
